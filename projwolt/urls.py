a0cb1486288d7aad80546ac7ff067b8e908d8088
Shalika Hiranthi Dissanayaka <ag1640@student.jamk.fi>
2025-05-05 02:48:25 +0300

    logout

--- projwolt/urls.py
+++ projwolt/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),  # Set home page URL
     path('earnings/', include('earnings.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
