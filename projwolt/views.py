fe8e4a5f0bafb09e3caeaf3e5cd502a5ac8b994f
Shalika Hiranthi Dissanayaka <ag1640@student.jamk.fi>
2025-05-05 08:43:23 +0300

    by 2 weeks

--- projwolt/views.py
+++ projwolt/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.db.models import Sum
 from django.db.models.functions import TruncMonth
-from earnings.models import Earning  # adjust as needed
+from earnings.models import Earning
+from collections import defaultdict
 
 def home(request):
     user = request.user
@@ -19,6 +20,24 @@ def home(request):
             .annotate(total=Sum('amount'))
             .order_by('month')
         )
+
+        earnings_by_half_month = defaultdict(lambda: {"start": 0, "end": 0})
+
+        if user.is_authenticated:
+            earnings = Earning.objects.filter(user=user)
+
+            for earning in earnings:
+                month_key = earning.date.strftime("%Y-%m")
+                if earning.date.day <= 15:
+                    earnings_by_half_month[month_key]["start"] += earning.amount
+                else:
+                    earnings_by_half_month[month_key]["end"] += earning.amount
+
+        # Convert to list of dicts sorted by month
+        sorted_earnings = sorted(
+            [{"month": k, **v} for k, v in earnings_by_half_month.items()],
+            key=lambda x: x["month"]
+        )
     else:
         total_earnings = 0
         monthly_earnings = []
@@ -26,4 +45,5 @@ def home(request):
     return render(request, 'home.html', {
         'total_earnings': total_earnings,
         'monthly_earnings': monthly_earnings,
+        'half_month_earnings': sorted_earnings
     })
